3f31de3b7602271c44c31d12e20a7ffc07affd13
Helapu <foolway@126.com>
2016-01-13 18:31:05 +0800

    remove

--- app/views/clients/note.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.array!(@notes) do |note|
-  json.extract! note, :id, :tip, :note
-  json.user note.user
-  json.url user_url(note.user, format: :json)
-end
--- db/migrate/20160104094816_add_contact_to_agents.rb
+++
@@ -1,8 +0,0 @@
-class AddContactToAgents < ActiveRecord::Migration
-  def change
-    create_table :agents_contacts, id: false do |t|
-      t.belongs_to :agent, index: true
-      t.belongs_to :contact, index: true
-    end
-  end
-end
