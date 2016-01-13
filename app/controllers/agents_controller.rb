c6aad971013cf4999622b7da61c6a40579c53779
Helapu <foolway@126.com>
2016-01-13 20:23:23 +0800

    agent show page

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,5 +1,16 @@
 class AgentsController < ResourcesController
+  def create_login
+    load_object
+    # 创建代理商登录帐号
+    c = Contact.find(params[:contact_id].to_i)
 
+    u = User.find_or_create_by("mobile"=>c.tel, "name"=>c.name, 'email'=> c.tel + "@pooul.cn")
+    if u
+      u.password = c.tel
+      u.agent = @object
+      u.save
+    end
+  end
   def del_salesman
     load_object
     s = Salesman.find(params[:salesman_id])
@@ -14,10 +25,17 @@ class AgentsController < ResourcesController
   end
 
   def show
+    load_object
+    if @object.company==nil
+      c = Company.create
+      @object.company = c
+      @object.save
+    end
+
     # 交易汇总
     @cur_trade_total = AgentDayTradetotal
             .select("sum(total_amount) as total_amount, sum(total_count) as total_count, sum(wechat_amount) as wechat_amount, sum(wechat_count) as wechat_count, sum(alipay_amount) as alipay_amount, sum(alipay_count) as alipay_count, sum(t0_amount) as t0_amount, sum(t0_count) as t0_count")
-            .where("trade_date"=>Date.new(2015,12,1).all_month, "agent_id"=> current_user.agent.id )
+            .where("trade_date"=>Date.new(2015,12,1).all_month, "agent_id"=> params[:id] )
             .group("id")
             .last
     #
