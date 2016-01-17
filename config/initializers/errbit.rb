d1d7590630ed8eed1fe308b164abcd7f6b6cab1b
jimmy <solo123@21cn.com>
2016-01-17 10:10:35 +0800

    errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
   config.host = 'http://error.pooul.cn'
   config.project_id = true
-  config.project_key = '04b89c299414a71c576bacd6f7c83f44'
-end
+  config.project_key = 'fe81ff2b35615e009656984060839f38'
+end if Rails.env.production?
