9dcfbc5eb06b6903951e8fff4892d8c854b7da24
jimmy <solo123@21cn.com>
2016-01-17 22:15:30 +0800

    agent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,25 @@ class ApplicationController < ActionController::Base
     end
 
 
-  before_action :require_agent_login
+  before_action :require_agent_login, :detect_browser
+
+private
+  def detect_browser
+    case request.user_agent
+      when /iPad/i
+        request.variant = :tablet
+      when /iPhone/i
+        request.variant = :phone
+      when /Android/i && /mobile/i
+        request.variant = :phone
+      when /Android/i
+        request.variant = :tablet
+      when /Windows Phone/i
+        request.variant = :phone
+      else
+        request.variant = :desktop
+    end
+  end
 
   def require_agent_login
     # 确保已经登录
