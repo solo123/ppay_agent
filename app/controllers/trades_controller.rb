a4e31bfc78cbae627a088c5682b8767cde712269
Helapu <foolway@126.com>
2016-01-18 16:43:32 +0800

    fixbug: pos信息空的处理

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -31,21 +31,23 @@ class TradesController < ApplicationController
   end
   def trade_detail(trade)
     client = Client.find(trade.client_id)
-    pos_machine = nil
-    if trade.pos_machine_id && trade.pos_machine_id > 0
-      pos_machine  = PosMachine.find(trade.pos_machine_id)
-    end
-
     clearing_type = "T1"
     if client.rate == 0.70
       clearing_type = "T0"
     end
-    return {"client.name"=> client.shop_name, "client.id"=> client.id, "client.url"=>client_path(client),
+    ret = {"client.name"=> client.shop_name, "client.id"=> client.id, "client.url"=>client_path(client),
             "sub_accont.name"=> trade.sub_account,
             "trade_date"=> trade.trade_date, "trade_type"=> trade.trade_type.name, "clearing_type"=> clearing_type,
              "rate"=> client.rate, "amount"=> trade.trade_amount, "trade.status"=> "ok",
-             "pos_machine.number"=> pos_machine.serial_number, "pos_machine.url"=>pos_machine_path(pos_machine)
            }
+  #
+    if trade.pos_machine_id && trade.pos_machine_id > 0
+     pos_machine  = PosMachine.find(trade.pos_machine_id)
+
+     ret["pos_machine.number"] = pos_machine.serial_number
+     ret["pos_machine.url"] = pos_machine_path(pos_machine)
+    end
+    return ret
   end
 
 end
