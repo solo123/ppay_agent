8fb2b0408be834a887cec78a82df1d640f5d1136
Helapu <foolway@126.com>
2016-01-21 18:18:52 +0800

    fix

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,5 +1,20 @@
 # coding: utf-8
 class AgentsController < ResourcesController
+
+  def show
+    load_object
+    agent_total = Biz::AgentTotalBiz.new params[:id]
+    puts '-' * 42
+    @cur_trade_total  = agent_total.trades_sum(Date.current)
+    puts @cur_trade_total
+
+    @cur_trade_total["clients_count"] = agent_total.clients_all.count
+    @cur_trade_total["new_clients_count"] = agent_total.new_clients.count
+    @cur_trade_total["company"] = Company.new
+    puts '-' * 42
+    puts @cur_trade_total
+  end
+
   def active_clients
     load_object
     total = Biz::AgentTotalBiz.new(@object.id)
