5db10600d2ca06d05dbef211d401370a0c200c1b
jimmy <solo123@21cn.com>
2016-02-24 12:21:26 +0800

    error

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,10 +1,2 @@
-class ContractsController < NestedResourcesController
-  before_action :edit_trade_sum_field, only: [:create, :update]
-
-  def edit_trade_sum_field
-    if params[:contract][:trade_sum]
-      params[:contract][:trade_sum] = CodeTable.find_code('trade_sum', params[:contract][:trade_sum])
-    end
-  end
-
+class ContractsController < ApplicationController
 end
